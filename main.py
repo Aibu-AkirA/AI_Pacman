be74b11beb51e79c352ba2b120d3c44e44f17cbf
Akira Aibu <wkdgml369@gmail.com>
2022-01-21 14:44:45 +0900

    [update]Add main screen output function

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+import sys
+import pygame
+from pygame.locals import *
+
+global screen
+global background
+pygame.init()
+
+screen = pygame.display.set_mode((600, 600))
+background = pygame.image.load("./image/mainScreen.jpg")
+pygame.display.set_caption("PAC-MAN")
+
+
 def main():
-    print("hello")
+
+    while True:
+        pygame.display.update()
+        screen.blit(background, (0, 0))
+        for event2 in pygame.event.get():
+            if event2.type == QUIT:
+                pygame.quit()
+                sys.exit
+
+    pygame.quit()
+    sys.exit
 
 
 if __name__ == '__main__':
